848f859f0ab83eb1741f9f7691029c6da06afeb9
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    minor css changes

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Statistics = () => {
-	return <div>Statistics</div>;
+	return (
+		<div className="page-container">
+			<h2>here is where the Home Feed Statistics will go</h2>
+			<div>
+				Bare minimum benchmark is to have a pie chart that shows the
+				distribution of genres/categories of all the submitted artworks by use
+				of chart.js. I can handle this <strong>- Madison</strong>
+			</div>
+		</div>
+	);
 };
 
 export default Statistics;
