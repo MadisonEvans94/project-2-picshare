dd8d2336a5bec31682f1d1d6e31cf249925a77df
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    standin like component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./Card.css";
 import testImage from "../../assets/test-images/test-image.jpg";
+import { useState } from "react";
+
 const Card = () => {
+	const [likes, setLikes] = useState(0);
+	/**
+	 * TODO: in future iteration, the likes will be persistent in database.
+	 * the like state pertains to singular instance of like. Will need to have a useEffect hook
+	 * that implements a patch request. page will optimistically render based on if user has
+	 * liked or disliked the post
+	 */
+
+	function handleLike() {
+		console.log("liked");
+	}
+
 	return (
 		<div className="card-container glass">
 			<div
@@ -14,6 +28,9 @@ const Card = () => {
 			<h2>Blue Cloud</h2>
 			<p>this is my artwork!</p>
 			<p>Genre: Futurism</p>
+			<div className="like-button" onClick={handleLike}>
+				+
+			</div>
 		</div>
 	);
 };
