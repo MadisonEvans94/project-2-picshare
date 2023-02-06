21170d9d9a01cf2915f46a94eeba1af66c366e71
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    checkpoint

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./Card.css";
+import testImage from "../../assets/test-images/test-image.jpg";
 const Card = () => {
 	return (
 		<div className="card-container glass">
-			<img src={"../../assets/test-images/test-image.jpg"} alt="test" />
+			<div
+				style={{
+					backgroundImage: `${testImage}`,
+					width: "200px",
+					height: "200px",
+					backgroundColor: "gray",
+				}}></div>
 			<h2>Blue Cloud</h2>
 			<p>this is my artwork!</p>
 			<p>Genre: Futurism</p>
