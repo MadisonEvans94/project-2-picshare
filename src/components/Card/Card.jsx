24df0988ffb33b590e58f5423a17260bdd43de2d
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    minor css changes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.css";
 const Card = () => {
 	return (
-		<div className="card-container">
+		<div className="card-container glass">
 			<img src={"../../assets/test-images/test-image.jpg"} alt="test" />
 			<h2>Blue Cloud</h2>
 			<p>this is my artwork!</p>
