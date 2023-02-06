5b268d8086973238b0a64752a2bcfb517158a1d8
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    standin functionality for genre updater in place

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import PieChart from "../../components/PieChart/PieChart";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Statistics = ({ masterList }) => {
-	const [formattedData, setFormattedData] = useState({
+	const [genreCount, setGenreCount] = useState([0, 0, 0, 0, 0, 0]);
+	const formattedData = {
 		labels: ["genre 1", "genre 2", "genre 3", "genre 4", "genre 5", "genre 6"],
 		datasets: [
 			{
 				label: "Distribution of Genres",
 
-				data: [12, 19, 3, 5, 2, 3],
+				data: genreCount,
 				backgroundColor: [
 					"rgba(255, 99, 132, 1)",
 					"rgba(54, 162, 235, 1)",
@@ -28,14 +29,15 @@ const Statistics = ({ masterList }) => {
 				borderWidth: 1,
 			},
 		],
-	});
-	function formatData(masterList) {
-		/**
-		 * take the database object: [{},{},{}] ... and use it to fill out the data attribute in formattedData
-		 
-		 */
-		return null;
+	};
+	function countGenres(masterList) {
+		//TODO: take in masterList and perform logic to count up all the genres and store them in the genreCount state
+		setGenreCount([0, 0, 0, 0, 0, 1]);
 	}
+	useEffect(() => {
+		countGenres(masterList);
+	}, [masterList]);
+
 	return (
 		<div className="page-container">
 			<h2>Statistics</h2>
