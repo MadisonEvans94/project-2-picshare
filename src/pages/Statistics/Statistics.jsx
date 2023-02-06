a5f6e20e191291808f3a7fa2748ac88f4b696d8f
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    dummy logic working for the statistics page

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,15 +1,49 @@
 import React from "react";
 import PieChart from "../../components/PieChart/PieChart";
 import { useState, useEffect } from "react";
+
+function countGenres(masterList) {
+	let tempGenres = {
+		genre1: 0,
+		genre2: 0,
+		genre3: 0,
+		genre4: 0,
+		genre5: 0,
+		genre6: 0,
+	};
+	masterList.forEach((obj) => {
+		switch (obj.cat) {
+			case "Sculpture":
+				tempGenres = { ...tempGenres, genre1: tempGenres.genre1 + 1 };
+				break;
+			case "Painting":
+				tempGenres = { ...tempGenres, genre2: tempGenres.genre2 + 1 };
+				break;
+			case "Pottery":
+				tempGenres = { ...tempGenres, genre3: tempGenres.genre3 + 1 };
+				break;
+			default:
+				break;
+		}
+	});
+	return tempGenres;
+}
 const Statistics = ({ masterList }) => {
-	const [genreCount, setGenreCount] = useState([0, 0, 0, 0, 0, 0]);
+	const [genreCount, setGenreCount] = useState({
+		genre1: 0,
+		genre2: 0,
+		genre3: 0,
+		genre4: 0,
+		genre5: 0,
+		genre6: 0,
+	});
 	const formattedData = {
 		labels: ["genre 1", "genre 2", "genre 3", "genre 4", "genre 5", "genre 6"],
 		datasets: [
 			{
 				label: "Distribution of Genres",
 
-				data: genreCount,
+				data: [],
 				backgroundColor: [
 					"rgba(255, 99, 132, 1)",
 					"rgba(54, 162, 235, 1)",
@@ -30,12 +64,10 @@ const Statistics = ({ masterList }) => {
 			},
 		],
 	};
-	function countGenres(masterList) {
-		//TODO: take in masterList and perform logic to count up all the genres and store them in the genreCount state
-		setGenreCount([0, 0, 0, 0, 0, 1]);
-	}
+
 	useEffect(() => {
-		countGenres(masterList);
+		const temp = countGenres(masterList);
+		setGenreCount(temp);
 	}, [masterList]);
 
 	return (
