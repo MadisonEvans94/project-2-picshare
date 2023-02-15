b2d13fcde9498b03d19b511fbae093c63bb706f9
Ziru Zou <jiyucoding@yahoo.com>
2023-02-14 19:22:37 -0500

    add search bar in find-by-artist page

--- src/App.js
+++ src/App.js
@@ -10,14 +10,23 @@ import { Routes, Route } from "react-router-dom";
 function App() {
 
 
-const [masterList, setMasterList] = useState([])
+	const [masterList, setMasterList] = useState([])
+	const [searchText, setSearchText] = useState("")
 
-useEffect (() => {
-	fetch("http://localhost:3200/items")
-	.then(res => res.json())
-	.then(data => setMasterList(data))
-},[])
+	useEffect (() => {
+		fetch("http://localhost:3200/items")
+		.then(res => res.json())
+		.then(data => setMasterList(data))
+	},[])
 
+
+	// Search by artist
+	function onHandleSearch(input) {
+		setSearchText(input)
+	}
+
+	const searchResults = masterList.filter(list => list.name.toLowerCase().includes(searchText.toLowerCase()))
+	console.log(searchResults)
 
 
 	return (
@@ -26,7 +35,7 @@ useEffect (() => {
 				<Nav />
 				<Routes>
 					<Route exact path="/" element={<Home masterList={masterList} />} />
-					<Route path="/find-by-user" element={<FindByUser />} />
+					<Route path="/find-by-artist" element={<FindByUser onHandleSearch={onHandleSearch} searchResults={searchResults}/>} />
 					<Route path="/statistics" element={<Statistics />} />
 				</Routes>
 			</Router>
